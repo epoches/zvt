a9273f6d5365beb532d75613555d24681d7f8327
epoches <epoches@sohu.com>
2021-10-14 17:32:01 +0800

    根据价格和ris买入

--- zvt/informer/informer.py
+++ zvt/informer/informer.py
@@ -160,10 +160,10 @@ class WechatInformer(Informer):
 
 if __name__ == '__main__':
     email_action = EmailInformer()
-    email_action.send_message(["5533061@qq.com", '2315983623@qq.com'], 'helo', 'just a test', sub_size=20)
+    email_action.send_message(["641279141@qq.com", '641279141@qq.com'], 'helo', 'just a test', sub_size=20)
 
-    # weixin_action = WechatInformer()
-    # weixin_action.send_price_notification(to_user='oRvNP0XIb9G3g6a-2fAX9RHX5--Q', security_name='BTC/USDT',
-    #                                       current_price=1000, change_pct='0.5%')
+    weixin_action = WechatInformer()
+    weixin_action.send_price_notification(to_user='oRvNP0XIb9G3g6a-2fAX9RHX5--Q', security_name='BTC/USDT',
+                                          current_price=1000, change_pct='0.5%')
 # the __all__ is generated
 __all__ = ['Informer', 'EmailInformer', 'WechatInformer']
